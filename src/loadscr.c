f9aed1c34bb08b35afe85b4229209dbea08118db
UNSTOP4BLE <88965856+UNSTOP4BLE@users.noreply.github.com>
2022-03-30 23:42:14 +0200

    loadscreen

--- src/loadscr.c
+++ src/loadscr.c
@@ -23,7 +23,7 @@ void LoadScr_Start(void)
 	
 	//Load loading screen texture
 	RECT loading_src = {0, 0, 255, 255};
-	RECT loading_dst = {(SCREEN_WIDTH - 300) >> 1, (SCREEN_HEIGHT - 224) >> 1, 300, 224};
+	RECT loading_dst = {(SCREEN_WIDTH - 320) >> 1, (SCREEN_HEIGHT - 240) >> 1, 320, 240};
 	Gfx_Tex loading_tex;
 	Gfx_SetClear(202, 255, 77);
 	Gfx_LoadTex(&loading_tex, IO_Read("\\MENU\\LOADING.TIM;1"), GFX_LOADTEX_FREE);
